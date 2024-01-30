ce606f2dc24e817b68a335d9f512d97f330b49a6
Madeleine Garbin <madeleine.garbin@kellogg.northwestern.edu>
2024-01-30 17:30:43 +0000

    lab

--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -9,9 +9,22 @@ Contact.destroy_all
 # **************************
 
 # - Insert and read contact data for companies in the database
+apple = Company.find_by({"name"=>"Apple"})
 
 # 1. insert new rows in the contacts table with relationship to a company
+new_contact=Contact.new
+new_contact["first_name"]="Tim"
+new_contact["last_name"]="Cook"
+new_contact["email"]="tcook@gmail.com"
+#assign that contact to Apple = using the foreign key of company id but I dont know it, but I want to row the primary key of a hash
+new_contact["company_id"]=apple["id"]
+new_contact.save
 
 # 2. How many contacts work at Apple?
+apple_contacts=Contact.where({"company_id"=>apple["id"]})
+puts "Apple contacts: #{apple_contacts.count}"
 
 # 3. What is the full name of each contact who works at Apple?
+for contact in apple_contacts
+    puts "#{contact["first_name"]}" "#{contact["las_name"]}"
+end
